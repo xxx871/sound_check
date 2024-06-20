a9a189f0e7a8350c30c14acb58f7e526face474c
xxx871 <0kh38c317264.7u@gmail.com>
2024-06-20 23:13:56 +0900

    お問い合わせ機能実装

---
+++ front/app/contact/page.tsx
@@ -0,0 +1,15 @@
+import ContactForm from '@/features/user/components/ContactForm'
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <main className="flex grow flex-col items-center justify-center">
+      <div className="space-y-4">
+        <h1 className="text-4xl">Contact Us</h1>
+        <ContactForm />
+      </div>
+    </main>
+  )
+}
+
+export default Contact
